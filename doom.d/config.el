64301db19e9a1d1fccbeeea238d35fded801357f
Ryan Ponce <ryan@ponce.la>
2018-12-10 07:49:48 -0500

    Initial commit

---
+++ doom.d/config.el
@@ -0,0 +1,7 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+(setq doom-theme 'doom-vibrant)
+
+(setq doom-font (font-spec :family "Dank Mono" :size 16))
